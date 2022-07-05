69f501fa506110ad8f65225b8936391b950966c2
Zoha-Tabassum <108558619+Zoha-Tabassum@users.noreply.github.com>
2022-07-05 07:08:57 +0530

    Create learn.java

---
+++ learn.java
@@ -0,0 +1,50 @@
+public class learn {
+	/** 
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	private String Conceptname;
+
+	/** 
+	 * @return the Conceptname
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public String getConceptname() {
+		// begin-user-code
+		return Conceptname;
+		// end-user-code
+	}
+
+	/** 
+	 * @param theConceptname the Conceptname to set
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void setConceptname(String theConceptname) {
+		// begin-user-code
+		Conceptname = theConceptname;
+		// end-user-code
+	}
+
+	/** 
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	private String Information;
+
+	/** 
+	 * @return the Information
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public String getInformation() {
+		// begin-user-code
+		return Information;
+		// end-user-code
+	}
+
+	/** 
+	 * @param theInformation the Information to set
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void setInformation(String theInformation) {
+		// begin-user-code
+		Information = theInformation;
+		// end-user-code
+	}
