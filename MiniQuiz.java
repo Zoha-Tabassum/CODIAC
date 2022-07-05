0ddea1f8d979a78ac95b7d27a98380357dd77b43
Zoha-Tabassum <108558619+Zoha-Tabassum@users.noreply.github.com>
2022-07-05 07:12:31 +0530

    Create MiniQuiz.java

---
+++ MiniQuiz.java
@@ -0,0 +1,65 @@
+public class MiniQuiz {
+	/** 
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	private Object ConceptName;
+
+	/** 
+	 * @return the ConceptName
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public Object getConceptName() {
+		// begin-user-code
+		return ConceptName;
+		// end-user-code
+	}
+
+	/** 
+	 * @param theConceptName the ConceptName to set
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void setConceptName(Object theConceptName) {
+		// begin-user-code
+		ConceptName = theConceptName;
+		// end-user-code
+	}
+
+	/** 
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	private Object TopicName;
+
+	/** 
+	 * @return the TopicName
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public Object getTopicName() {
+		// begin-user-code
+		return TopicName;
+		// end-user-code
+	}
+
+	/** 
+	 * @param theTopicName the TopicName to set
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void setTopicName(Object theTopicName) {
+		// begin-user-code
+		TopicName = theTopicName;
+		// end-user-code
+	}
+
+	/** 
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	private Object Question;
+
+	/** 
+	 * @return the Question
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public Object getQuestion() {
+		// begin-user-code
+		return Question;
+		// end-user-code
+	}
